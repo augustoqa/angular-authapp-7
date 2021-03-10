76c62b5ebc859c4c19e3c00b4cd826907d1a6987
César Acual <augustoacual@gmail.com>
2021-03-10 12:03:49 -0600

    Mostrar botones para login para ingresar y proteger ruta protegida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HomeComponent } from './components/home/home.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
 import { PreciosComponent } from './components/precios/precios.component';
 
+import { AuthModule } from '@auth0/auth0-angular';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +20,11 @@ import { PreciosComponent } from './components/precios/precios.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-iub8qlh6.us.auth0.com',
+      clientId: 'GhysD2T49h6K1r7kE1KaASqvl8rF4IBj'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
